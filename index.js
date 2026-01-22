42b2696ae2a369b3c146bb6fbf8dac21e5d0e8ab
edivanrabelo <edivanrabelo9@gmail.com>
2026-01-21 21:18:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("olá mundão")
+
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas
+    let nivel = ""
+
+    if (vitorias < 10) {
+        nivel = "Ferro"
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze"
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata"
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro"
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante"
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário"
+    } else if (vitorias >= 101) {
+        nivel = "Imortal"
+    }
+
+    return { saldoVitorias, nivel }
+}
+
+let jogadores = [
+    { vitorias: 120, derrotas: 20 },
+    { vitorias: 55, derrotas: 10 },
+    { vitorias: 18, derrotas: 5 }
+]
+
+for (let i = 0; i < jogadores.length; i++) {
+    let resultado = calcularNivel(jogadores[i].vitorias, jogadores[i].derrotas);
+console.log("o heroi tem o saldo de " + resultado.saldoVitorias + "está no de " + resultado.nivel)
+}
